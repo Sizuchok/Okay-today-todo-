7e8221d719a4420b2460ecc33f76146414e0b993
liiusion <37956664+Sizuchok@users.noreply.github.com>
2019-04-07 15:21:04 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'todolist'
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log('DB connected!');
+});
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    let sql = 'select * from todolist1';
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        res.render('index', {todoLs: result});
+    });
+});
+
+app.post('/newtodo', (req, res) => {
+    var item = req.body.item;
+    let sql = `insert todolist1(ToDo, Completed) values('${item}', 0)`;
+    connection.query(sql, (err, res) =>{
+        if (err) throw err;
+    });
+    res.redirect('/');
+});
+
+app.get('/delete', (req, res) => {
+    let sql = `delete from todolist1 where id = ${req.query.id}`;
+    connection.query(sql, (err, result) =>{
+        if(err) throw err;
+        res.send();
+    });
+});
+
+app.get('/isdone', (req, res) => {
+    let sql = `update todolist1 set Completed = ${req.query.status} where ID = ${req.query.id}`;
+    connection.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send();
+    });
+});
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+document.addEventListener('click', (event) => {
+    if(event.target.id == 'del'){
+        var liel = event.target;
+        liel = liel.parentElement;
+        liel = liel.parentElement;
+
+        var id = liel.id;
+
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                liel.parentNode.removeChild(liel);
+            }
+        }
+
+        request.open("GET", `/delete?id=${id}`);
+        request.send();
+    }
+});
+
+document.addEventListener('click', (event) => {
+    if(event.target.id == 'check' || event.target.id == 'uncheck'){
+
+        var status = 0, sendstatus = 1;
+        if(event.target.id == 'check'){
+            status =  1;
+            sendstatus = 0;
+        }
+
+        var liel = event.target;
+        liel = liel.parentElement;
+        liel = liel.parentElement;
+        var id = liel.id;
+
+        var request = new XMLHttpRequest();
+
+        request.onreadystatechange = () => {
+            if(request.readyState == 4 && request.status == 200){
+                var el = event.target;
+                var newEl = document.createElement('img');
+                if(status){
+                    newEl.setAttribute('id', 'uncheck');
+                    newEl.setAttribute('src', '/assets/img/uncheck.png');
+                    liel.removeAttribute('class');
+                    el.parentNode.replaceChild(newEl, el);
+                }
+                else{
+                    newEl.setAttribute('id', 'check');
+                    newEl.setAttribute('src', '/assets/img/check.png');
+                    liel.setAttribute('class', 'doneli');
+                    el.parentNode.replaceChild(newEl, el);
+                }
+            }
+        };
+
+        request.open('GET', `/isdone?id=${id}&status=${sendstatus}`);
+        request.send();
+    }
+});
